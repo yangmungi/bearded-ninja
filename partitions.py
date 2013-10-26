649a7a97ecaf138e03676f77429b2cceceb666f6
Yangmun Choi <yangmungi@gmail.com>
2013-10-26 15:49:47 -0500

    Adding partitions script

---
+++ partitions.py
@@ -0,0 +1,46 @@
+#!/bin/python
+"""
+Partitioning of numbers.
+"""
+
+import sys
+
+def departition(base, parent = None):
+    num = base
+
+    part = []
+    while num > 0:
+        sub_part = [num]
+
+        sub_part_sum = sum(sub_part)
+        diff = base - sub_part_sum
+
+        if parent is None or num <= parent:
+          if sub_part_sum < base:
+              de_part = departition(diff, num)
+              for de_sub_part in de_part:
+                  sub_part = sub_part + de_sub_part
+                  part.append(sub_part)
+                  sub_part = [num]
+          else:
+              part.append(sub_part)
+        num -= 1;
+    return part
+        
+if len(sys.argv) == 2:
+    base = int(sys.argv[1])
+else:
+    base = 7
+
+print "Start with %d " % 10
+
+sizes = []
+
+for i in xrange(base):
+    departitions = departition(i)
+
+    sizes.append(len(departitions))
+    for depart in departitions:
+        print depart
+    print 
+print sizes
