8902e4e7fefa37ae347b46891210f3f885a15a28
Yangmun Choi <yangmungi@ucla.edu>
2014-01-09 22:00:40 -0800

    min-squares - minimizing output.

--- min-squares.py
+++ min-squares.py
@@ -3,19 +3,29 @@ import sys
 import logging
 
 def min_squares(width, height):
-    """Determine the minimum number of squares used in a rectangle."""
+    """
+    Determine the minimum number of squares used in a rectangle.
+    """
     if width <= 0 or height <= 0:
         return 0
 
     logging.debug("%s x %s" % (width, height))
+
     mod_r = height
     low = width
 
+    # Total square count
     count = 0
+
+    # Used for debugging
     areas = 0
 
     while mod_r != 0:
         iterable = [low, mod_r]
+
+        # Think of this as an inversion, where low is now the height and
+        # hig is the width of a rectangle that is always longer than it
+        # is high
         low = min(iterable)
         hig = max(iterable)
 
@@ -27,11 +37,16 @@ def min_squares(width, height):
 
         diff = count - oc
         areas += low * low * diff
-
-        logging.info("%s [%s] total, %s X (%s x %s [%s]), %s x %s left" % (count, areas, diff, low, low, (areas - oa), low, hig))
+        logging.info("%s [%s] total, %s X (%s x %s [%s]), %s x %s left" % 
+            (count, areas, diff, low, low, (areas - oa), low, hig))
     return count
 
 if __name__ == "__main__":
+    """
+    Arg 1: Width (default 3)
+    Arg 2: Height (default 7)
+    """
+
     w = 3
     len_argv = len(sys.argv)
     if len_argv >= 2:
@@ -41,7 +56,15 @@ if __name__ == "__main__":
     if len_argv >= 3:
         h = int(sys.argv[2])
         
+    min_most = -1
+    smallest_wh = (-1, -1)
+
     # The big test is to do this in constant time?
     for i in xrange(w): 
         min_c = min_squares(i, h)
-        print "%s x %s" % (i, h), min_c
+        if min_c != 0 and (min_most == -1 or min_c < min_most):
+            smallest_wh = (i, h)
+            min_most = min_c
+
+
+    print "%s squares" % min_c
